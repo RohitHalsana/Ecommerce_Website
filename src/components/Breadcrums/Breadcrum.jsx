0d3908810d8492e138dd5369108cf99c030731bb
RohitHalsana <rohithalsana08@gmail.com>
2025-07-17 21:54:03 +0530

    Breadcrum

--- src/components/Breadcrums/Breadcrum.jsx
+++ src/components/Breadcrums/Breadcrum.jsx
@@ -5,8 +5,8 @@ import arrow_icon from '../Assets/breadcrum_arrow.png'
 const Breadcrum = (props) => {
     const{product} = props;
     return (
-    <div className="breadcrum">
-        HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" />  {product.category} <img src={arrow_icon} alt="" /> {product.name}
+    <div className="breadcrum">        
+        Home <img src={arrow_icon} alt="" /> Shop <img src={arrow_icon} alt="" />  {product.category} <img src={arrow_icon} alt="" /> {product.name}
     </div>
   )
 }
